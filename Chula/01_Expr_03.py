822cad33f12730a5436d039318787c9efd881809
Thanakrit Trongmahavisate <thanakrit.natty@gmail.com>
2021-08-10 15:27:20 +0700

    updated

--- Chula/01_Expr_03.py
+++ Chula/01_Expr_03.py
@@ -1,6 +1,6 @@
 import math
 
-upper = math.pi - (math.factorial(10)/8**8) + (math.log(9.7))**((7/math.sqrt(71))-math.sin(40))
+upper = math.pi - (math.factorial(10)/8**8) + (math.log(9.7))**((7/math.sqrt(71))-math.sin(40*(math.pi/180)))
 lower = (1.2) ** (2.3 ** (1/3))
 ans = upper/lower
 
