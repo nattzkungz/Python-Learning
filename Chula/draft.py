1c86f1ba2e0582d296d69178b7e61dd28ea765f8
Thanakrit Trongmahavisate <thanakrit.natty@gmail.com>
2021-08-15 13:35:50 +0700

    popcat token

--- Chula/draft.py
+++ Chula/draft.py
@@ -1,1 +1,6 @@
-print(9**2)
+import math
+
+n = int(input("A number"))
+
+print(math.sqrt(2*(math.pi)) * n**(n+1/2) * math.e**(-n + 1/((12*n)+1)))
+print(math.sqrt(2*(math.pi)) * n**(n+1/2) * math.e**(-n + 1/((12*n))))
--- popcatfun.py
+++ popcatfun.py
@@ -6,7 +6,7 @@ from random import randint
 scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
 
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJDb3VudHJ5Q29kZSI6IlRIIiwiQ291bnRyeU5hbWUiOiJUaGFpbGFuZCIsIklQIjoiMTgwLjE4My4xNTMuMjMiLCJJRCI6MzAsImV4cCI6MTYyOTA0OTU0M30.ZHk25AqVJcibQQI2_FhNYgOENmFcRL-zcQ5Njd3zc-Y"
-captcha_token = "MWRqemt1aDEzZXVpYWE="
+captcha_token = "05AGTLn29S1LEKo9BJtpgetKvVJzPuYg20lzoQ-qpCGlm3Jnvlh5CYiUe0bxRq2_vJ7x5hrTb93k5XytEgG-u14anguL3V6H1RJFsAmM57Vni62LkndwAZQftJ6QjItDRrvT6bbN0Xj_r7uTnSluN-EJCv6DOnOBrpm6Qpq6CfUY_HO90HLyqtuDDC69CWMmnhEEStaDyayPulDBQdV1mekPmUMJk"
 clicked = 0
 for i in range(9999999999):
 	url = f"https://stats.popcat.click/pop?pop_count=800&captcha_token={captcha_token}&token={token}"
